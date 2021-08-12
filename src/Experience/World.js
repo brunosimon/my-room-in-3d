441a478714115299e5f555b3e6c0ad4840271b73
brunosimon <simon.bruno.77@gmail.com>
2021-08-12 23:51:17 +0200

    :construction: Test model

--- src/Experience/World.js
+++ src/Experience/World.js
@@ -14,18 +14,31 @@ export default class World
         {
             if(_group.name === 'base')
             {
-                this.setDummy()
+                // this.setDummy()
+                this.setRoom()
             }
         })
     }
 
-    setDummy()
+    // setDummy()
+    // {
+    //     const cube = new THREE.Mesh(
+    //         new THREE.BoxGeometry(1, 1, 1),
+    //         new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+    //     )
+    //     this.scene.add(cube)        
+    // }
+
+    setRoom()
     {
-        const cube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
-        )
-        this.scene.add(cube)        
+        this.room = {}
+        this.room.model = this.resources.items.roomModel.scene
+
+        this.scene.add(this.room.model)
+
+        const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+        directionalLight.position.set(5, 5, 5)
+        this.scene.add(directionalLight)
     }
 
     resize()
--- src/Experience/assets.js
+++ src/Experience/assets.js
@@ -5,6 +5,7 @@ export default [
         items:
         [
             { name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture' },
+            { name: 'roomModel', source: '/assets/roomModel.glb' },
         ]
     }
 ]
